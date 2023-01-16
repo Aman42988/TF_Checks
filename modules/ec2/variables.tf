da55f50d9a24ffb1c9e45d17827ae1eba091873d
Nandita Sahu <22912146+NanditaSahu03@users.noreply.github.com>
2023-01-16 22:06:57 +0530

    Create variables.tf

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,24 @@
+variable "name" { 
+  type    =  string
+  default = ""
+}
+
+variable "ENV" {
+  type    = string
+  default = ""
+}
+
+variable "instance_type" {
+  type    = string
+  default = ""
+}
+
+variable "aws_security_group" {
+  type    = list(string)
+  default = []
+}
+
+variable "iam_instance_profile" {
+  type    = string
+  default = ""
+}
