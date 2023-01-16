8d31f06387b9cbcc8fb021e6e1e58e6e3e5880e7
Nandita Sahu <22912146+NanditaSahu03@users.noreply.github.com>
2023-01-16 22:05:59 +0530

    Create ec2.tf

---
+++ modules/ec2/ec2.tf
@@ -0,0 +1,37 @@
+data "aws_ami" "ubuntu_22_0" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-groovy-22.0-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu_22_0.id
+  instance_type          = var.instance_type
+  key_name               = "${var.name}-Instance"
+  vpc_security_group_ids = var.aws_security_group
+  
+  root_block_device {
+      encrypted = true
+  }
+  
+  monitoring             = true   
+  iam_instance_profile   = var.iam_instance_profile
+
+   tags = {
+    Name = "${var.name}-Terraform-Instance"
+    POD  = var.POD
+  }
+ 
+  #tfsec:ignore:aws-ec2-enforce-http-token-imds 
+  metadata_options {
+    
+  }
+}
