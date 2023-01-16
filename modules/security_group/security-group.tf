0ce8d0a10e5ab54b4016b8013e61dbc6162c9cb1
Nandita Sahu <22912146+NanditaSahu03@users.noreply.github.com>
2023-01-16 22:08:29 +0530

    Create security-group.tf

---
+++ modules/security_group/security-group.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "allow_sg" {
+  name        = "${var.name}-SG"     
+  description = "Allow tls inbound traffic"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = var.cidr_blocks
+  }
+  
+   ingress {
+    description      = "Jfrog Port from VPC"
+    from_port        = 8082
+    to_port          = 8082
+    protocol         = "tcp"
+    cidr_blocks      = var.cidr_blocks
+  }
+
+  egress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]  #tfsec:ignore:aws-ec2-no-public-egress-sgr
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-SG",
+    ENV  = var.ENV
+  }
+}
